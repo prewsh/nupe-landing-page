cd4d5091cf537bf9fc2dc742066b3e8facd5dccc
prewsh <pngwube@gmail.com>
2020-07-17 05:46:26 +0100

    done

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+import flash
+# import yaml
+
+app = Flask(__name__)
+
+# Configure db
+# db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = 'localhost'
+# db['mysql_host']
+app.config['MYSQL_USER'] = 'root'
+# db['mysql_user']
+app.config['MYSQL_PASSWORD'] = '1234'
+# db['mysql_password']
+app.config['MYSQL_DB'] = 'flaskapp'
+# db['mysql_db']
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Fetch form data
+        userDetails = request.form
+        email = userDetails['email'] 
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(email) VALUES(%s)",[email])
+        mysql.connection.commit()
+        cur.close()
+        # flash('You were successfully suscribed')  
+        return redirect('/')
+    return render_template('index.html')
+
+@app.route('/subscribers')
+def users(): 
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM users")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template('subscribers.html',userDetails=userDetails)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -25,7 +25,7 @@ setInterval(function () {
     seconds = parseInt(seconds_left % 60);
      
     // format countdown string + set tag value
-    countdown.innerHTML = '<span class="days">' + days +  ' <label>Days</label></span> <span class="hours">' + hours + ' <label>Hours</label></span> <span class="minutes">'
+    countdown.innerHTML = '<span class="days">' + days  +  ' <label>Days</label></span> <span class="hours">' + hours + ' <label>Hours</label></span> <span class="minutes">'
     + minutes + ' <label>Minutes</label></span> <span class="seconds">' + seconds + ' <label>Seconds</label></span>';  
  
 }, 1000);
@@ -37,3 +37,25 @@ $(function() {
     new SimpleLightbox('.app-pics .big', { /* options */ });
 })
 
+
+// scroll to top js here 
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
